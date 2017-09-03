6ed4a6cf1eabfefe779450b65a55afbd8e08ad65
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-09-03 12:08:19 -1000

    DELETE CARD action added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,23 @@
+let nextCardId = 0;
+
 export const ADD_CARD = 'ADD_CARD';
-export const REMOVE_CARD = 'REMOVE_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
 
 export const addCard = (card) => {
   return {
     type: ADD_CARD,
-    card: card
+    id: nextCardId++,
+    status: 'queue',
+    ...card
   };
 };
+
+export const deleteCard = (id) => {
+  return {
+    type: DELETE_CARD,
+    id
+  };
+};
+
+
+

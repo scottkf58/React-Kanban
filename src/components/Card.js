6ee822b38e7eeef79ed559aa267fc83e0db5c69a
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-09-03 12:29:50 -1000

    reformat

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,13 @@ import React from 'react';
 
 const Card = ({ id, title, priority, createdBy, assignedTo, handleDelete }) =>
   <div className={priority}>
-    <div>
-      {title}
-    </div>
-    <div>
-      {priority}
-    </div>
-    <div>
-      {assignedTo}
-    </div>
-    <div>
-      {createdBy}
-    </div>
+    <div>{title}</div>
+
+    <div>{priority}</div>
+
+    <div>{assignedTo}</div>
+
+    <div>{createdBy}</div>
     <div onClick={handleDelete} id={id} className="link">
       DELETE
     </div>

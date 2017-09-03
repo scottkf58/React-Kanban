64296256f5e10d4fbf5e34b20bce9b525a5344fc
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-09-03 12:09:14 -1000

    added card properties

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Card = ({id, title}) => {
-  return (
-    <div className="card-item">
-      <p>id</p>
-      <p>title</p>
-
+const Card = ({ id, title, priority, createdBy, assignedTo, handleDelete }) =>
+  <div className={priority}>
+    <div>
+      {title}
     </div>
-  )
-}
+    <div>
+      {priority}
+    </div>
+    <div>
+      {assignedTo}
+    </div>
+    <div>
+      {createdBy}
+    </div>
+    <div onClick={handleDelete} id={id} className="link">
+      DELETE
+    </div>
+  </div>;
+
+export default Card;
